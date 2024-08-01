a78c2f685dcbe5fdd1f306b0d2628f5d525714d8
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-08-01 11:33:37 +0300

    move base url to env variable

--- lib/core/utils/constants/strings.dart
+++ lib/core/utils/constants/strings.dart
@@ -1,5 +1,6 @@
 // Networking and APIs
-const String baseUrl = 'http://10.0.2.2:3000/api/';
+const String baseUrl = String.fromEnvironment('BASE_URL',
+    defaultValue: 'http://localhost:3000/api/');
 
 // Storage and database
 const String articlesTableName = 'articles_table';
