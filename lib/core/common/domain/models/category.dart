eb4661391479383ccab074628ee6d53f894be48d
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-08-02 09:27:31 +0300

    feat: add optional count in category model

--- lib/core/common/domain/models/category.dart
+++ lib/core/common/domain/models/category.dart
@@ -2,16 +2,20 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'category.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class Category {
   final int id;
   final String name;
   final String? description;
 
+  @JsonKey(name: '_count')
+  final CategoryCount? count;
+
   Category({
     required this.id,
     required this.name,
-    required this.description,
+    this.description,
+    this.count,
   });
 
   factory Category.fromJson(Map<String, dynamic> json) =>
@@ -23,4 +27,16 @@ class Category {
   String toString() {
     return 'Category{id: $id, name: $name, description: $description}';
   }
+}
+
+@JsonSerializable()
+class CategoryCount {
+  final int quizzes;
+
+  CategoryCount({required this.quizzes});
+
+  factory CategoryCount.fromJson(Map<String, dynamic> json) =>
+      _$CategoryCountFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CategoryCountToJson(this);
 }
--- lib/core/common/domain/models/category.g.dart
+++ lib/core/common/domain/models/category.g.dart
@@ -10,10 +10,24 @@ Category _$CategoryFromJson(Map<String, dynamic> json) => Category(
       id: (json['id'] as num).toInt(),
       name: json['name'] as String,
       description: json['description'] as String?,
+      count: json['_count'] == null
+          ? null
+          : CategoryCount.fromJson(json['_count'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$CategoryToJson(Category instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'description': instance.description,
+      '_count': instance.count?.toJson(),
+    };
+
+CategoryCount _$CategoryCountFromJson(Map<String, dynamic> json) =>
+    CategoryCount(
+      quizzes: (json['quizzes'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$CategoryCountToJson(CategoryCount instance) =>
+    <String, dynamic>{
+      'quizzes': instance.quizzes,
     };
