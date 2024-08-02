7856c72d8e4c004a9a32776d6386f54bbda8d1f4
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-08-02 12:10:34 +0300

    feat: add question stats

--- lib/features/features_nyandriax/question_management/presentation/pages/detail_question_page.dart
+++ lib/features/features_nyandriax/question_management/presentation/pages/detail_question_page.dart
@@ -5,7 +5,7 @@ import 'package:q_learn/core/common/domain/models/question.dart';
 import 'package:q_learn/core/common/widgets/app_bar_back_button.dart';
 
 @RoutePage()
-class DetailQuestionPage extends StatelessWidget {
+class DetailQuestionPage extends StatefulWidget {
   final Question question;
 
   const DetailQuestionPage({
@@ -13,37 +13,11 @@ class DetailQuestionPage extends StatelessWidget {
     required this.question,
   });
 
-  void _showDeleteConfirmationDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Confirmer la suppression'),
-          content:
-              const Text('Êtes-vous sûr de vouloir supprimer cette question ?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Ferme la boîte de dialogue
-              },
-              child: const Text('Annuler'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Ajouter la logique de suppression ici
-                Navigator.of(context).pop(); // Ferme la boîte de dialogue
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Question supprimée')),
-                );
-              },
-              child: const Text('Supprimer'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+  @override
+  State<DetailQuestionPage> createState() => _DetailQuestionPageState();
+}
 
+class _DetailQuestionPageState extends State<DetailQuestionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,48 +25,50 @@ class DetailQuestionPage extends StatelessWidget {
         title: const Text('Détail de la question'),
         leading: const AppBarBackButton(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Question',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.copyWith(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            Text(question.content),
-            const SizedBox(height: 16),
-            Text(
-              'Réponses',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.copyWith(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            buildAnswers(),
-            const SizedBox(height: 16),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Question',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(widget.question.content),
+              const SizedBox(height: 16),
+              Text(
+                'Réponses',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              ...buildAnswers(),
+              const SizedBox(height: 16),
+              ...buildStatics(),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget buildAnswers() {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: question.answers.length,
-        itemBuilder: (context, index) {
-          return buildAnswerDetail(
-            question.answers[index],
-          );
-        },
-      ),
-    );
+  List<Widget> buildAnswers() {
+    return [
+      ...List.generate(
+        widget.question.answers.length,
+        (index) => buildAnswerDetail(
+          widget.question.answers[index],
+        ),
+      )
+    ];
   }
 
   Widget buildAnswerDetail(Answer answer) {
@@ -101,6 +77,7 @@ class DetailQuestionPage extends StatelessWidget {
       elevation: 0,
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+        width: MediaQuery.of(context).size.width,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -111,5 +88,57 @@ class DetailQuestionPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget buildStat(String title, {dynamic value = 0.0}) {
+    return Card(
+      color: Colors.blueGrey.shade50,
+      elevation: 0,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title),
+            const SizedBox(height: 10),
+            Text(
+              '${value ?? 0}',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildStatics() {
+    return [
+      Text(
+        'Statistiques',
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge
+            ?.copyWith(fontWeight: FontWeight.bold),
+      ),
+      const SizedBox(height: 8),
+      buildStat(
+        "Nombre de fois où la question a été répondue",
+        value: widget.question.count?.count,
+      ),
+      const SizedBox(height: 8),
+      buildStat(
+        "Nombre de fois où la question a été correctement répondue",
+        value: widget.question.count?.success,
+      ),
+      const SizedBox(height: 8),
+      buildStat(
+        "Pourcentage de succès",
+        value: widget.question.count?.rate,
+      ),
+    ];
   }
 }
