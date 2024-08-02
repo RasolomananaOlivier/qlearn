674df5c5f623cab506c3e7c04197191df2a53e44
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-08-02 11:26:15 +0300

    style: remove elevation in admin dashboard

--- lib/features/features_nyandriax/admin_dashboard/presentation/pages/admin_dashboard_page.dart
+++ lib/features/features_nyandriax/admin_dashboard/presentation/pages/admin_dashboard_page.dart
@@ -40,102 +40,104 @@ class _AdminDashboardPageState extends ConsumerState<AdminDashboardPage> {
 class _BuildBody extends StatelessWidget {
   final void Function(int) onItemTapped;
 
-  const _BuildBody({super.key, required this.onItemTapped});
+  const _BuildBody({
+    super.key,
+    required this.onItemTapped,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-            side: const BorderSide(color: Colors.blueAccent, width: 2.0),
-          ),
-          elevation: 8,
-          margin: const EdgeInsets.only(bottom: 16.0),
-          child: const ListTile(
-            contentPadding: EdgeInsets.all(20.0),
-            title: Text(
-              'Total Questions',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              side: const BorderSide(color: Colors.blueAccent, width: 2.0),
             ),
-            trailing: Text(
-              '45',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-            side: const BorderSide(color: Colors.greenAccent, width: 2.0),
-          ),
-          elevation: 8,
-          margin: const EdgeInsets.only(bottom: 16.0),
-          child: const ListTile(
-            contentPadding: EdgeInsets.all(20.0),
-            title: Text(
-              'Total Quiz',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            ),
-            trailing: Text(
-              '12',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            elevation: 0,
+            margin: const EdgeInsets.only(bottom: 16.0),
+            child: const ListTile(
+              contentPadding: EdgeInsets.all(20.0),
+              title: Text(
+                'Total Questions',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              trailing: Text(
+                '45',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
-        ),
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0),
-            side: const BorderSide(color: Colors.redAccent, width: 2.0),
-          ),
-          elevation: 8,
-          margin: const EdgeInsets.only(bottom: 16.0),
-          child: const ListTile(
-            contentPadding: EdgeInsets.all(20.0),
-            title: Text(
-              'Total Categories',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              side: const BorderSide(color: Colors.greenAccent, width: 2.0),
             ),
-            trailing: Text(
-              '0',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            elevation: 0,
+            margin: const EdgeInsets.only(bottom: 16.0),
+            child: const ListTile(
+              contentPadding: EdgeInsets.all(20.0),
+              title: Text(
+                'Total Quiz',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              trailing: Text(
+                '12',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 24.0),
-        const Text(
-          'Gestion de contenu',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 16.0),
-        _buildContentSection('Categorie', 1),
-        _buildContentSection('Question', 3),
-        _buildContentSection('Quiz', 2),
-      ],
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              side: const BorderSide(color: Colors.redAccent, width: 2.0),
+            ),
+            elevation: 0,
+            margin: const EdgeInsets.only(bottom: 16.0),
+            child: const ListTile(
+              contentPadding: EdgeInsets.all(20.0),
+              title: Text(
+                'Total Categories',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              trailing: Text(
+                '0',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const SizedBox(height: 24.0),
+          const Text(
+            'Gestion de contenu',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16.0),
+          _buildContentSection('Categorie', 1),
+          _buildContentSection('Question', 3),
+          _buildContentSection('Quiz', 2),
+        ],
+      ),
     );
   }
 
   Widget _buildContentSection(String title, int index) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-        side: const BorderSide(color: Colors.grey, width: 1.0),
-      ),
-      elevation: 4,
-      margin: const EdgeInsets.only(bottom: 12.0),
-      child: ListTile(
-        contentPadding: const EdgeInsets.all(16.0),
-        title: Text(
-          title,
-          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+    return GestureDetector(
+      onTap: () => onItemTapped(index),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          side: const BorderSide(color: Colors.grey, width: 1.0),
         ),
-        trailing: ElevatedButton(
-          onPressed: () {
-            onItemTapped(index);
-          },
-          child: const Text('Visiter'),
+        elevation: 0,
+        margin: const EdgeInsets.only(bottom: 12.0),
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(16.0),
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
         ),
       ),
     );
