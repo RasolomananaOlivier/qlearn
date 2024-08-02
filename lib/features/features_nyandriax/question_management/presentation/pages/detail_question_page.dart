bd10eeecb430c5f388de34ab4011846059e11bf0
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-08-02 09:02:04 +0300

    feat: update question details UI

--- lib/features/features_nyandriax/question_management/presentation/pages/detail_question_page.dart
+++ lib/features/features_nyandriax/question_management/presentation/pages/detail_question_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:q_learn/core/common/domain/models/answer.dart';
 import 'package:q_learn/core/common/domain/models/question.dart';
+import 'package:q_learn/core/common/widgets/app_bar_back_button.dart';
 
 @RoutePage()
 class DetailQuestionPage extends StatelessWidget {
@@ -44,18 +46,10 @@ class DetailQuestionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String content = question.content;
-    final List<dynamic> answers = question.answers;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Détail de la question'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
+        leading: const AppBarBackButton(),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -63,87 +57,56 @@ class DetailQuestionPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Question: $content',
+              'Question',
               style: Theme.of(context)
                   .textTheme
                   .titleLarge
                   ?.copyWith(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            // Text(
-            //   'Difficulté: $difficulty',
-            //   style: Theme.of(context)
-            //       .textTheme
-            //       .titleMedium
-            //       ?.copyWith(color: Colors.grey[700]),
-            // ),
-            // const SizedBox(height: 8),
-            // Text(
-            //   'Catégorie ID: $categoryId',
-            //   style: Theme.of(context)
-            //       .textTheme
-            //       .titleMedium
-            //       ?.copyWith(color: Colors.grey[700]),
-            // ),
+            Text(question.content),
             const SizedBox(height: 16),
-            const Text(
-              'Réponses:',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            Text(
+              'Réponses',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            // Flexible(
-            //   child: ListView.builder(
-            //     itemCount: answers.length,
-            //     itemBuilder: (context, index) {
-            //       final answer = answers[index];
-            //       final answerText =
-            //           answer is Answer ? answer.content : answer.toString();
-            //       final isCorrect = answer is Answer ? answer.isCorrect : false;
-
-            //       return Card(
-            //         color: isCorrect ? Colors.green : Colors.blue,
-            //         child: Container(
-            //           padding: const EdgeInsets.all(8.0),
-            //           child: Center(
-            //             child: Text(
-            //               answerText,
-            //               style: const TextStyle(
-            //                 color: Colors.white,
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //       );
-            //     },
-            //   ),
-            // ),
+            buildAnswers(),
             const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigator.of(context).push(
-                    //   MaterialPageRoute(
-                    //     builder: (context) => CreationQuestionPage(
-                    //       question: question,
-                    //     ),
-                    //   ),
-                    // );
-                  },
-                  child: const Text('Modifier'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _showDeleteConfirmationDialog(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
-                  child: const Text('Supprimer'),
-                ),
-              ],
-            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildAnswers() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: question.answers.length,
+        itemBuilder: (context, index) {
+          return buildAnswerDetail(
+            question.answers[index],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildAnswerDetail(Answer answer) {
+    return Card(
+      color: Colors.blueGrey.shade50,
+      elevation: 0,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(answer.content),
+            const SizedBox(height: 10),
+            Text('Bonne réponse : ${answer.isCorrect ? 'Oui' : 'Non'}'),
           ],
         ),
       ),
