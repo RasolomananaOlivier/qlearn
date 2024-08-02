9620c697413d78058c5a8e9fb3e0cae99a8ffc23
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-08-02 09:15:32 +0300

    fix: category model

---
+++ lib/core/common/domain/models/category.dart
@@ -0,0 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'category.g.dart';
+
+@JsonSerializable()
+class Category {
+  final int id;
+  final String name;
+  final String? description;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      _$CategoryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CategoryToJson(this);
+
+  @override
+  String toString() {
+    return 'Category{id: $id, name: $name, description: $description}';
+  }
+}
